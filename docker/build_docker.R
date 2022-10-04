51dc1c4b5631aa32f82fdf5d47a64547d0df7841
DammitJim <eminty@gmail.com>
2022-10-04 13:57:41 -0600

    initial docker build

---
+++ docker/build_docker.R
@@ -0,0 +1,6 @@
+# NOTE: before running this script be sure to set the 
+# working directory to the location of the Dockerfile
+
+bashstring <- "sudo docker build -t eminty/ApPhenotypeEvaluation:0.1 ."
+system(bashstring)
+
